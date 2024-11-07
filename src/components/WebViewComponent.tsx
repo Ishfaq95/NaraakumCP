66874df0c7f0b33ac7bc99cf612f9b20bf16e6d7
Muhammad Ishfaq <ishfaqyasin969@gmail.com>
2024-11-07 12:38:13 +0500

    web url change

--- src/components/WebViewComponent.tsx
+++ src/components/WebViewComponent.tsx
@@ -139,7 +139,7 @@ const WebViewComponent = ({ uri }:any) => {
 
     if (url && url.includes('OnlineSessionRoom')) {
       setCallConnected(true)
-      let urlComplete = `https://dev2.innotech-sa.com${url}`;
+      let urlComplete = `https://dvx.innotech-sa.com${url}`;
       // let urlComplete = `https://staging.innotech-sa.com${url}`;
       // let urlComplete = `https://nkapps.innotech-sa.com${url}`;
       const redirectUrl = getDeepLink();
